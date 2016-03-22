bf70753faea2bff6b5ba6aac69fb535261e506ad
roman <romanlozanoe@gmail.com>
2016-03-21 22:38:47 -0600

    Initial Commit

---
+++ pruebaGit2Tests/pruebaGit2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pruebaGit2Tests.swift
+//  pruebaGit2Tests
+//
+//  Created by Roman Lozano Elizondo on 3/21/16.
+//  Copyright © 2016 Roman Lozano Elizondo. All rights reserved.
+//
+
+import XCTest
+@testable import pruebaGit2
+
+class pruebaGit2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
